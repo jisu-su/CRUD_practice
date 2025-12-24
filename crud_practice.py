743899586c87ead5413d5cb26954bc39dc388800
박지수 <trleresoo@gmail.com>
2025-12-24 17:46:14 +0900

    특정 줄 삭제하기

--- crud_practice.py
+++ crud_practice.py
@@ -2,6 +2,14 @@
 # 1. 특정 줄만 삭제하기
 # 2. 파일 전체 삭제하기
 
-file = open("instructor.txt","a",encoding="utf-8")
-file.write("\n 특정 줄 삭제를 위한 줄 추가 \n 메리크리스마스")
+# 수동일 경우에는 읽기 - 삭제 - 다시쓰기
+
+file = open("instructor.txt","r",encoding="utf-8")
+lines = file.readlines()
+file.close()
+
+del (lines[1])
+
+file = open("instructor.txt","w",encoding="utf-8")
+file.writelines(lines)
 file.close()
