f904e56dbd5da6df2cbcd173c85f2784a6622cea
박지수 <trleresoo@gmail.com>
2025-12-24 17:54:42 +0900

    파일 삭제 완료

--- crud_practice.py
+++ crud_practice.py
@@ -2,14 +2,7 @@
 # 1. 특정 줄만 삭제하기
 # 2. 파일 전체 삭제하기
 
-# 수동일 경우에는 읽기 - 삭제 - 다시쓰기
-
-file = open("instructor.txt","r",encoding="utf-8")
-lines = file.readlines()
-file.close()
-
-del (lines[1])
-
-file = open("instructor.txt","w",encoding="utf-8")
-file.writelines(lines)
-file.close()
+# 파일 전체 삭제할 경우 os 도구를 빌려와야 한다.
+#1.
+import os
+os.remove("instructor.txt")
