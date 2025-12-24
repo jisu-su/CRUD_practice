ba4fb5be8e5898c0c7550944312ab45e5581672e
박지수 <trleresoo@gmail.com>
2025-12-24 16:23:14 +0900

    'a'를 이용해서 말을 추가해보았다.

--- crud_practice.py
+++ crud_practice.py
@@ -1,6 +1,7 @@
-# Read 차례. 이제는 메세지를 읽어볼 거다
+# update. 'a'랑 'w' 둘 다 사용해보자.
+# 'a'는 뒤에 추가하는 방식. append
+# 'w'는 덮어쓰기 write
 
-file = open("instructor.txt", "r", encoding="utf-8")
-content = file.read()
-print(content)
+file = open("instructor.txt", "a", encoding="utf-8")
+file.write("\n영어학습어플에서도 미남이라서 신기했습니다")
 file.close()
