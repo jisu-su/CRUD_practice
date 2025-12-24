91e10e564ef377728a144d15c122196e91b0b2c9
박지수 <trleresoo@gmail.com>
2025-12-24 16:36:42 +0900

    'a'추가 후 'r'로 확인

--- crud_practice.py
+++ crud_practice.py
@@ -2,6 +2,7 @@
 # 'a'는 뒤에 추가하는 방식. append
 # 'w'는 덮어쓰기 write
 
-file = open("instructor.txt", "a", encoding="utf-8")
-file.write("\n영어학습어플에서도 미남이라서 신기했습니다")
+file = open("instructor.txt", "r", encoding="utf-8")
+content = file.read()
 file.close()
+print(content)
