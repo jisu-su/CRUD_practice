62d219b36a079b33cad2ca5a2bc177c46e624928
박지수 <trleresoo@gmail.com>
2025-12-26 16:40:17 +0900

    a 모드로 가사 추가하기

--- crud_12_26_practice.py
+++ crud_12_26_practice.py
@@ -1,7 +1,9 @@
 # 저번 시간에 적어본 crud 수동을 자동으로 적어보기
 # 오늘의 주제는 노래 가사 이어 말하기
-# create 부분. file = open 이 아닌 with -as - 사용해보기
+# update 부분. "a", "r" 모드로 추가해보기
+# 1. "a" 모드로 추가
 
-with open("lyrics.txt", "w", encoding="utf-8") as file:
-    file.write("late night your eyes fell to tme floor\n")
-    file.write("i'm trying to make sense but you're losing your patience")
+with open("lyrics.txt", "a", encoding="utf-8") as file:
+    file.write("\nhands talk won't stop we go to war")
+    file.write("\nin the heat of the moment you think that we're broken")
+    file.write("\ni could see my whole life with you baby")
