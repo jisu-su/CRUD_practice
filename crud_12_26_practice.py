860b0200160fd5dba144a1ea61f0728db1c5d4e6
박지수 <trleresoo@gmail.com>
2025-12-26 17:50:36 +0900

    텍스트 파일 삭제 성공

--- crud_12_26_practice.py
+++ crud_12_26_practice.py
@@ -1,9 +1,13 @@
 # 저번 시간에 적어본 crud 수동을 자동으로 적어보기
 # 오늘의 주제는 노래 가사 이어 말하기
 # delete부분. 
-# 1. 특정 줄 삭제하기
+# 2. 파일 자체 삭제하기
 
-with open("lyrics.txt", "r", encoding="utf-8") as file:
-    lyrics_data = file.read()
-    print(lyrics_data)
+import os
+if os.path.exists("lyrics.txt"):
+    os.remove("lyrics.txt")
+    print("lyrics.txt 파일이 삭제되었습니다.")
+else:
+    print("lyrics.txt 파일이 존재하지 않습니다.")
+
 
