8877bfd96166aa96237a2f83a0b1ebb4365d9dfd
박지수 <trleresoo@gmail.com>
2025-12-26 17:46:49 +0900

    특정 줄 삭제하기 성공

--- crud_12_26_practice.py
+++ crud_12_26_practice.py
@@ -3,8 +3,7 @@
 # delete부분. 
 # 1. 특정 줄 삭제하기
 
-with open("lyrics.txt", "w", encoding="utf-8") as file:
-    file.write("아무 일 없던 것처럼 이 모든 게 난 다 꿈일 거리고")
-    file.write("\n눈을 다시 감고 떴을 땐 안심하며 꺨 아침이길 바랬어")
-    file.write("\n어긋나 버린 우리 미래에 시간을 거슬러 갈 수 있다면")
-    file.write("\n거칠기보단 따뜻하게 널 부르며 보내줄 수 없을까")
+with open("lyrics.txt", "r", encoding="utf-8") as file:
+    lyrics_data = file.read()
+    print(lyrics_data)
+
