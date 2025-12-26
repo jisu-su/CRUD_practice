e939c7ba86b5b360b39da4706c26202a59a77fee
박지수 <trleresoo@gmail.com>
2025-12-26 16:53:19 +0900

    읽기 모드 실행

--- crud_12_26_practice.py
+++ crud_12_26_practice.py
@@ -3,7 +3,6 @@
 # update 부분. "a", "r" 모드로 추가해보기
 # 1. "a" 모드로 추가
 
-with open("lyrics.txt", "a", encoding="utf-8") as file:
-    file.write("\nhands talk won't stop we go to war")
-    file.write("\nin the heat of the moment you think that we're broken")
-    file.write("\ni could see my whole life with you baby")
+with open("lyrics.txt", "r", encoding="utf-8") as file:
+    lyrics_data = file.read()
+    print(lyrics_data)
