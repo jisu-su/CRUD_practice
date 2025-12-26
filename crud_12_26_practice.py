3fd3bccdd082f756c72ea9902dbb4eac8af74258
박지수 <trleresoo@gmail.com>
2025-12-26 17:38:32 +0900

    줄바꿈 장치 때문에 4번째 줄이 날아갔다

--- crud_12_26_practice.py
+++ crud_12_26_practice.py
@@ -4,10 +4,5 @@
 # 1. 특정 줄 삭제하기
 
 with open("lyrics.txt", "r", encoding="utf-8") as file:
-    lines = file.readlines()
-
-if len(lines) > 2: # 혹시 줄이 없을 때를 대비한 안전장치
-    del lines[2] 
-
-with open("lyrics.txt", "w", encoding="utf-8") as file:
-    file.writelines(lines)  
+    lyrics_data = file.read()
+    print(lyrics_data)
